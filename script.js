ad452d3dad35e393badad02f0081ae267e328458
PANxxJ <ps789875@gmail.com>
2023-07-19 22:40:36 +0530

    f

--- script.js
+++ script.js
@@ -7,5 +7,27 @@ const changeTurn= () => {
     return turn==='X'?'O':'X';
 }
 const changeWin = () => {
-    return null
+    let wins=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
 }
+
+let boxes=document.getElementsByClassName('box')
+Array.from(boxes).forEach(element =>{
+    let boxtext=element.querySelector('.boxtext')
+    element.addEventListener('click', ()=>{
+        if (boxtext.innerText===''){
+            boxtext.innerText=turn
+            turn=changeTurn()
+            // turn_audio.play()
+            document.getElementsByClassName('info')[0].innerText='Turn for '+turn
+        }
+    })
+})
